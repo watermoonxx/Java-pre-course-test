163b47d05dc11d78bd7a20d7444eca86b8a10037
Jun <watermoonxx@gmail.com>
2022-11-08 04:40:48 +0900

    Add files via upload

---
+++ src/Problem1.java
@@ -0,0 +1,17 @@
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		
+		char[] alphaArray = new char[26];
+		int convert = 65;
+		
+		for (int i=0; i<alphaArray.length; i++) {
+			alphaArray[i] = (char) convert;
+			convert++;
+			System.out.println(alphaArray[i]);
+		}
+		
+	}
+	
+}
